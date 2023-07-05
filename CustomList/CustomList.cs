0611d9edac80cab278790077fcea62e59e8ac43e
skyeler yearsley <s2krighty@gmail.com>
2023-07-04 22:36:00 -0500

    initial commit

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        //Member Variables (HAS A)
+        private T[] items;
+        private int capacity;
+        private int count;
+
+        //Constructor
+        public CustomList()
+        {
+            capacity = 4;
+            count = 0;
+            items = new T[capacity];
+        }
+
+        //Member Methods (CAN DO)
+        public void Add(T item)
+        {
+            //'item' parameter should be added to internal 'items' array
+            //if items array is at capacity, double capacity and create new array
+            //transfer all items to new array
+        }
+
+        public bool Remove(T item)
+        {
+            //If 'item' exists in the 'items' array, remove its first instance
+            //Any items coming after the removed item should be shifted down so there is no empty index.
+            //If 'item' was removed, return true. If no item was removed, return false.
+            return false;
+        }
+
+        public override string ToString()
+        {
+            //returns a single string that contains all items from array
+            return "";
+        }
+
+        public static CustomList<T> operator +(CustomList<T> firstList, CustomList<T> secondList)
+        {
+            //returns a single CustomList<T> that contains all items from firstList and all items from secondList 
+            return null;
+        }
+
+        public static CustomList<T> operator -(CustomList<T> firstList, CustomList<T> secondList)
+        {
+            //returns a single CustomList<T> with all items from firstList, EXCEPT any items that also appear in secondList
+            return null;
+        }
+
+
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
